d3b90d7cfe823616b8b152d6431ad3252e9db479
Ondřej Vaculík <35863250+vaculiko@users.noreply.github.com>
2021-12-13 18:10:39 +0100

    Kostra pro tvorbu hry Hangman

---
+++ hangman.py
@@ -0,0 +1,14 @@
+zivoty = 7
+hra_bezi = True
+slovo = "obesenec" # TODO: Náhodný výběr slov pomocí random.choice()
+tajenka = len(slovo) * ["_"]
+
+# 1. Smyčka pro celou hru, hlídání životů
+
+# 2. Hádání slova/písmena - input()
+
+# 3. Vyhodnocení výsledku hry
+
+# TODO: Úklid výpisu v konzoli - os.system("cls")
+
+# TODO: Vykreslení figurky
